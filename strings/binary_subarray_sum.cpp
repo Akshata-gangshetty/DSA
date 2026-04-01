bdd46b0059f22c5d077c4bdd996f8b0b50aa136b
r <rk@gmail.com>
2026-04-01 09:01:44 -0700

    committed

---
+++ strings/binary_subarray_sum.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binary_subarray_sum(vector<int>arr,int k){
+    int l=0,r=0,sum=0,cnt=0,maxlen=0;
+    while (r<arr.size())
+    {
+       sum+=arr[r];
+       while (sum>k)
+       {
+            sum=sum-arr[l];
+            l=l+1;
+       }
+       cnt=cnt+(r-l+1);
+       r++;
+       
+    }
+    return cnt;
+    
+}
+int main(){
+    vector<int>arr={1,1,1,0,1,1,0};
+    int maxlen=binary_subarray_sum(arr,2);
+    cout<<maxlen<<" ";
+}
