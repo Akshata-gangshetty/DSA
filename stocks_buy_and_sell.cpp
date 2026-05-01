535d340ac869f1c62b39c411c0ef843a28e738ee
Akshata Gangshetty <akshata.vangshetty@gmail.com>
2026-05-01 09:33:26 -0700

    commited

---
+++ stocks_buy_and_sell.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+//TC:O(n) and Sc:O(1)
+int maxprofit(vector<int>price){
+    int mini=price[0];
+    int profit=0;
+    int n=price.size();
+    for (int i = 0; i < n; i++)
+    {
+        int cost=price[i]-mini;
+        profit=max(profit,cost);
+        mini=min(mini,price[i]);
+        
+    }
+    return profit;
+    
+}
+int main(){
+    vector<int>price={5,1,7,4,3,6};
+    int profit=maxprofit(price);
+    cout<<profit;
+    return 0;
+
+}
