2f67933653c48d5313a3da38efcca470ca1567eb
Akshata-gangshetty <akshata.vgangshetty@gmail.com>
2026-05-09 09:39:52 -0700

    Update count_num_nice_subarray.cpp

--- strings/count_num_nice_subarray.cpp
+++ strings/count_num_nice_subarray.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+//TC:O(N) and SC:O(1)
 int nice_subarray(vector<int>arr,int k){
     int l=0,r=0,maxlen=0,cnt=0,sum=0;
     while(r<arr.size()){
         sum=sum+(arr[r]%2);
         while (sum>k)
         {
-           sum=sum-(arr[l]%2);
+           sum=sum-(arr[l]%2);//it only increment never decrease or 0 and so max level reach is N so it both loop execute N times
            l++;
         }
         cnt+=(r-l+1);
