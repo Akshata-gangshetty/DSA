e101afb6b4e4d70a5d14db7e91a226f46c7dce59
Akshata Gangshetty <akshata.vangshetty@gmail.com>
2026-07-09 08:34:05 -0700

    committed

---
+++ strings/SUB_ARRAY_SUM.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int SubarraywithSum(vector<int>& nums,int goal)
+{
+    if(goal < 0) return 0;
+    int l=0, r=0, sum=0,count=0;
+    while( r < nums.size())
+    {
+        sum += nums[r];
+        while(sum > goal)
+        {
+            sum -= nums[l];
+            l=l+1;
+        }
+        count += (r-l+1);
+        r=r+1;
+    }
+    return count;
+}
+int main()
+{
+    vector<int> nums = {1,0,0,1,1,0};
+    int goal = 2;
+    cout << "Number of subarrays: "<< SubarraywithSum(nums,goal) << endl;
+    return 0;
+
+}
