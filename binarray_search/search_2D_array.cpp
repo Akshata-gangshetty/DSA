c14129e434d4955c443d8e6d197c3657eccf8eb6
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-07-27 03:50:46 -0700

    committed

---
+++ binarray_search/search_2D_array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int low=0;long long high(n*m -1);
+        while(low<=high){
+           long long mid=(low+high)/2;
+            
+            int val=matrix[mid/n][mid%n];
+            if(val==target) return true;
+            else if(val<=target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+        
+    }
+};
