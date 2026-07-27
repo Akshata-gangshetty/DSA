46257777ddc03bc293bb6f680f0f1108b8c42642
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-07-27 03:10:32 -0700

    committed

---
+++ arrays_qp/row_max_ones.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int maxones=0;
+        int rowindex=0;
+        for(int i=0;i<mat.size();i++){
+            int cnt=0;
+            for(int x:mat[i]){
+                cnt+=x;
+            }
+            if(cnt>maxones){
+                maxones=cnt;
+                rowindex=i;
+            }
+        }
+        return {rowindex,maxones};
+    }
+};
