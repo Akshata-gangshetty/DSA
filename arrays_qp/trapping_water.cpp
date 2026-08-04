8dcdf0f5df7a98d52ad66e22a61b29e1aabbdaf5
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-08-04 05:09:47 -0700

    committed

---
+++ arrays_qp/trapping_water.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+        int n=arr.size();
+        int l=0,r=n-1;
+        int total=0;
+        int lmax=0,rmax=0;
+        while(l<r){
+            if(arr[l]<=arr[r]){
+                if(arr[l]<lmax){
+                    total+=lmax-arr[l];
+                }
+                else{
+                    lmax=arr[l];
+                }
+                l++;
+            }
+            else{
+                if(arr[r]<rmax){
+                    total+=rmax-arr[r];
+                }
+                else{
+                    rmax=arr[r];
+                }
+                r--;
+            }
+
+        }
+        return total;
+        
+    }
+};
