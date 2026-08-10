8ee389228c47085e476cf6b968900b4f0633e269
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-08-09 23:00:19 -0700

    committed

---
+++ stack/online_stock_price.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+    int ind;
+    int ans;
+    StockSpanner() {
+        ind=-1;
+        
+    }
+    
+    int next(int price) {
+        ind=ind+1;
+        
+        while(!st.empty() && st.top().first <=price){
+            st.pop();
+          
+        } int ans = st.empty() ? ind + 1 : ind - st.top().second;
+           
+         st.push({price,ind});
+        return ans;
+    }
+};
+
