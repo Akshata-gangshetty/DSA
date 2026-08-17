50e2451d53a0812fd3ccfd04cf3e9d48ae8de0bd
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-08-17 07:31:33 -0700

    committed

---
+++ trees/inorder_traversal__iterative.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+    vector<int> inorderTraversal(TreeNode* root) {
+      
+        stack<TreeNode*>st;
+        vector<int>ans;
+        TreeNode* node=root;
+        while(node !=nullptr || !st.empty()){
+          while(node !=nullptr){
+            st.push(node);
+             node=node->left;
+            
+          }
+          node=st.top();
+          st.pop();
+          ans.push_back(node->val);
+          node =node->right;
+            
+        }
+        return ans;
+       
+    }
+};
