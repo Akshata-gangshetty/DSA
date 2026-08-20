2eb43ee3dcb67fd6b2ef0d4b346844a8770fc5f5
AbhishekGangshetty <abhishekgangshetty@gmail.com>
2026-08-20 01:13:11 -0700

    committed

---
+++ trees/tree_len.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int findmax(TreeNode* root,int &maxi){
+    if(!root) return 0;
+        int lh=findmax(root->left,maxi);
+        int rh=findmax(root->right,maxi);
+        maxi=max(maxi,lh+rh);
+        return 1+max(lh,rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+      int len= findmax(root,maxi);
+      return maxi;
+    }
+};
